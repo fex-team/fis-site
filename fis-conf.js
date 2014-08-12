185b6a74844dea6895771d424737290c9563a433
hefangshi <hefangshi@gmail.com>
2014-08-12 20:48:51 +0800

    fix domain

--- fis-conf.js
+++ fis-conf.js
@@ -5,8 +5,8 @@ fis.config.set('roadmap.path', [
 	}
 ]);
 
-fis.config.set('roadmap.domain', 'http://fis.baidu.com');
-fis.config.set('roadmap.domain', 'http://127.0.0.1:8080');
+// fis.config.set('roadmap.domain', 'http://fis.baidu.com');
+fis.config.set('roadmap.domain', 'http://hefangshi.github.io/fis-site');
 fis.config.set('modules.postpackager', 'simple');
 fis.config.set('modules.optimizer.html', 'html-minifier');
 fis.config.set('settings.postpackager.simple.autoCombine', true);
