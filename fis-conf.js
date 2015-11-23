ffbc101bb614a15ed133573617ce0ee95ee6fc05
hefangshi <hefangshi@gmail.com>
2015-11-23 15:32:53 +0800

    fix https

--- fis-conf.js
+++ fis-conf.js
@@ -3,11 +3,11 @@ fis.config.set('roadmap.path', [{
     useDomain: true
 }]);
 
-fis.config.set('roadmap.domain', 'https://fex.baidu.com/fis-site');
+fis.config.set('roadmap.domain', 'http://fex.baidu.com/fis-site');
 // fis.config.set('roadmap.domain', 'http://hefangshi.github.io/fis-site');
 fis.config.set('modules.postpackager', 'simple');
 fis.config.set('modules.optimizer.html', 'html-minifier');
-fis.config.set('settings.postpackager.simple.autoCombine', true);
+fis.config.set('settings.postpackager.simple.autoCo zbine', true);
 fis.config.set('settings.optimizer.html-minifier', {
     minifyJS: true
 });
