12505ff1a3f1e99533086cdf4a2b98ff89e73fd3
hefangshi <hefangshi@baidu.com>
2016-08-30 19:45:22 +0800

    change url

--- fis-conf.js
+++ fis-conf.js
@@ -4,7 +4,6 @@ fis.config.set('roadmap.path', [{
 }]);
 
 fis.config.set('roadmap.domain', 'http://fex-team.github.io/fis-site');
-// fis.config.set('roadmap.domain', 'http://hefangshi.github.io/fis-site');
 fis.config.set('modules.postpackager', 'simple');
 fis.config.set('modules.optimizer.html', 'html-minifier');
 fis.config.set('settings.postpackager.simple.autoCo zbine', true);
